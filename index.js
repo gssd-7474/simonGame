5a6b547cf26a4fa9a4b38ce17a6c48ae8d5889a8
gssd-7474 <gauravsood31july@gmail.com>
2020-09-01 18:33:25 +0530

    .

---
+++ index.js
@@ -0,0 +1,90 @@
+var buttonColors = ["red", "blue", "yellow", "green"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+
+
+
+$(document).keydown(function(event) {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+
+});
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+  // nextSequence();
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("sucess");
+    if (userClickedPattern.length === gamePattern.length) {
+
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed")
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed")
+  }, 100);
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChooseColors = buttonColors[randomNumber];
+
+  gamePattern.push(randomChooseColors);
+
+  $("#" + randomChooseColors).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChooseColors);
+  animatePress(randomChooseColors);
+
+
+
+}
+
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
